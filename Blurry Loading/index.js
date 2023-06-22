aa597d22b56df46114ab6547b6d79d8d30d6f384
Abujdnyaneshwari <84312485+Abujdnyaneshwari@users.noreply.github.com>
2023-06-22 23:04:40 +0530

    Implemented Blurry Loading

---
+++ Blurry Loading/index.js
@@ -0,0 +1,29 @@
+const bgImage = document.querySelector('.bg')
+const loadingText = document.querySelector('.loading-text')
+
+let load = 0
+let interval
+
+
+function startBlurring(){
+    interval = setInterval(blurring, 30)
+}
+
+
+function blurring(){
+    load++
+
+    if(load > 99){
+        clearInterval(interval)
+    }
+
+    loadingText.innerText = `${load}%`
+    loadingText.style.opacity = scale(load, 0, 100, 1, 0)
+    bgImage.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+  }
+
+  startBlurring()
